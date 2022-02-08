38b96ef5f6050227d0602890a730963de8c3af35
RusuVasile0309 <vasile@noteamyet.com>
2022-02-08 11:19:47 +0200

    it works

--- src/RadarComponents.jsx
+++ src/RadarComponents.jsx
@@ -7,16 +7,32 @@ const basicFormat = format('.2r');
 const wideFormat = format('.3r');
 
 const RadarComponent = ({ domains, data }) => {
-
+    const borderMaxData = {
+        name: 'Max',
+        Speed: 30,
+        Power: 100,
+        Luck: 777,
+        Attitude: 10,
+        Autonomy: 50,
+        Range: 180,
+        Weight: 120.8,
+        Battery: 2500,
+        Awesomeness: 100,
+        Fair: 2.5,
+        Logical: 10,
+        Bald: 50,
+        fill: 'grey',
+        stroke: 'rgba(114,172,240,1)'
+    }
     return (
         <div style={{ marginLeft: "60px", marginTop: "200px" }}>
             <RadarChart
                 style={{
                     axes: {
                         line: {
-                            fillOpacity: 0.8,
+                            // fill: "black",
                             strokeWidth: 0.5,
-                            strokeOpacity: 0.8
+                            stroke: "black"
                         },
                         ticks: {
                             fillOpacity: 1,
@@ -34,7 +50,7 @@ const RadarComponent = ({ domains, data }) => {
                     }
 
                 }}
-                data={data}
+                data={[...data, borderMaxData]}
                 tickFormat={t => basicFormat(t)}
                 startingAngle={0}
                 renderAxesOverPolygons={true}
